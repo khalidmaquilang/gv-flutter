6305237a52e758500c66d7a2f0c79510372acb15
khalidmaquilang <120788000+khalidmaquilang@users.noreply.github.com>
2025-12-19 02:25:23 +0800

    Added id

--- lib/features/profile/data/models/profile_video_model.dart
+++ lib/features/profile/data/models/profile_video_model.dart
@@ -1,9 +1,11 @@
 class ProfileVideo {
+  final String id;
   final String thumbnail;
   final String privacy;
   final int views;
 
   ProfileVideo({
+    required this.id,
     required this.thumbnail,
     required this.privacy,
     required this.views,
@@ -11,6 +13,7 @@ class ProfileVideo {
 
   factory ProfileVideo.fromJson(Map<String, dynamic> json) {
     return ProfileVideo(
+      id: json['id']?.toString() ?? '',
       thumbnail: json['thumbnail'] ?? '',
       privacy: json['privacy'] ?? 'public',
       views: json['views'] ?? 0,
